2d31100d725941359ba0afe5ed15c8b89abe624b
jhonsanchezgomez <48131958+jhonsanchezgomez@users.noreply.github.com>
2019-03-19 14:59:06 -0500

    Add files via upload

---
+++ src/main/java/com/microservicio/service/ActualizarMsApplication.java
@@ -0,0 +1,22 @@
+package com.microservicio.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ActualizarMsApplication {
+	
+	@Bean
+	public RestTemplate getWebClient(){
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActualizarMsApplication.class, args);
+	}
+
+}
